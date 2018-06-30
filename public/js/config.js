abd38171d93973e81104cf17f0398ac038cd4bae
ArlinL <junlin.luo@istuary.com>
2018-06-30 13:04:37 +0800

    aa

--- public/js/config.js
+++ public/js/config.js
@@ -10,6 +10,6 @@ var SAMPLE_SERVER_BASE_URL = 'https://wjvideo.herokuapp.com';
 // set these values to OpenTok API key, a valid session ID, and a token for the session.
 // For test purposes, you can obtain these from https://tokbox.com/account.
 
-var API_KEY = '';
-var SESSION_ID = '';
-var TOKEN = '';
+var API_KEY = '46144322';
+var SESSION_ID = '2_MX40NjE0NDMyMn5-MTUzMDMzMjIxMzExMH4xb1dJd0ViaDVwL1NOWXV0cWwyYjM1cUl-fg';
+var TOKEN = 'T1==cGFydG5lcl9pZD00NjE0NDMyMiZzaWc9MmYyYjAxNjdiZTg1ZTY0NDg3MTZiZTI2MGE0YWI1ZTQxNDUxM2JhMDpzZXNzaW9uX2lkPTJfTVg0ME5qRTBORE15TW41LU1UVXpNRE16TWpJeE16RXhNSDR4YjFkSmQwVmlhRFZ3TDFOT1dYVjBjV3d5WWpNMWNVbC1mZyZjcmVhdGVfdGltZT0xNTMwMzMyMjEzJm5vbmNlPTAuMTExMTQ2Nzc5MzEwOTE1OTkmcm9sZT1wdWJsaXNoZXImZXhwaXJlX3RpbWU9MTUzMDQxODYxMyZpbml0aWFsX2xheW91dF9jbGFzc19saXN0PQ==';
